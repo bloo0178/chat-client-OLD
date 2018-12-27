c917e16280280b285dc89926da65c3e987e74f25
bloo0178 <bloo0178@gmail.com>
2018-12-27 17:54:24 -0500

    Added delete channel and default operator users to createChannel().

--- src/components/Channels/Channels.js
+++ src/components/Channels/Channels.js
@@ -2,17 +2,53 @@ import React from 'react';
 import ChannelList from './ChannelList';
 import CreateChannel from './CreateChannel';
 import DeleteChannel from './DeleteChannel';
+import JoinChannel from './JoinChannel';
 
+class Channels extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedChannel: '',
+            refreshChannels: false
+        }
+    }
 
-const Channels = (props) => {
-    return (
-        <div>
-            Channels Main
-            <ChannelList />
-            <CreateChannel />
-            <DeleteChannel />
-        </div>
-    )
+    // setSelectedChannel() is used to obtain the channelURL from the channelList
+    // component. selectedChannel state is passed to JoinChannel and DeleteChannel
+    // to perform the respective operations based on which button is clicked.
+    setSelectedChannel = (channel) => {
+        this.setState({
+            selectedChannel: channel
+        })
+    }
+
+    // refreshChannels() is called from DeleteChannel when a user deletes a channel
+    // they are an admin of. refreshChannels is passed to ChannelList as a key.
+    // When this value changes, it prompts ChannelList to refresh - reflecting
+    // a new list of channels without the channel that was just deleted.
+    refreshChannels = () => {
+        this.setState({
+            refreshChannels: !this.state.refreshChannels
+        })
+    }
+
+    render() {
+        console.log(this.props.history);
+        console.log(this.state.refreshChannels);
+        return (
+            <div>
+                <ChannelList
+                    key={this.state.refreshChannels}
+                    setSelectedChannel={this.setSelectedChannel} />
+                <JoinChannel selectedChannel={this.state.selectedChannel} history={this.props.history} />
+                <DeleteChannel
+                    refreshChannels={this.refreshChannels}
+                    selectedChannel={this.state.selectedChannel}
+                    history={this.props.history} />
+                <CreateChannel />
+            </div >
+        )
+    }
 }
 
 export default Channels;
--- src/components/Channels/DeleteChannel.js
+++ src/components/Channels/DeleteChannel.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import { connect } from 'react-redux';
 
 class DeleteChannel extends React.Component {
     constructor(props) {
         super(props)
     }
 
+    handleClick = () => {
+            this.props.sb.OpenChannel.getChannel(this.props.selectedChannel, (channel, error) => {
+                if (error) { 
+                    return console.log(error);     
+                 }
+                channel.delete((response, error) => {
+                    if (error) { 
+                        console.log(error); 
+                        return alert('You are not an admin of the channel you are trying to delete.');
+                    }
+                    alert("Channel deleted.");
+                    this.props.refreshChannels();
+                })
+            })
+        }
+
     render() {
         return (
-            <div>
-                delete goes here
-            </div>
+            <Button size="sm" onClick={this.handleClick}>Delete</Button>
         )
     }
 }
 
-export default DeleteChannel;
+const mapStateToProps = state => {
+    return {
+        sb: state.sbsession.sbsession
+    }
+}
+
+export default connect(mapStateToProps)(DeleteChannel);
