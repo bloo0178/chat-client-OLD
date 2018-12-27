864af35f43edab6438ede74559ee4301f8d777b2
bloo0178 <bloo0178@gmail.com>
2018-12-27 11:25:39 -0500

    Added create channel functionality.

--- src/components/Channels/Channels.js
+++ src/components/Channels/Channels.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ChannelList from './ChannelList';
+import CreateChannel from './CreateChannel';
+
 
 const Channels = (props) => {
     return (
         <div>
             Channels Main
             <ChannelList />
+            <CreateChannel />
         </div>
     )
 }
--- src/components/Channels/CreateChannel.js
+++ src/components/Channels/CreateChannel.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { clearMessages, setChannelURL } from '../../actions'
+import { Input, Button } from 'reactstrap';
+import { BrowserRouter as Router, Redirect, Link } from 'react-router-dom';
 
 class CreateChannel extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            name: '',
+            channelCreated: false
+        }
     }
 
-    // Will need to change this to take an input to replace 'TEST_CHANNEL'
-    handleClick = () => {
-        this.props.sb.OpenChannel.createChannel('TEST_CHANNEL', null, null, null, (channel, error) => {
-            if (error) {
-                return;
-            }
-            console.log('channel created');
-            console.log(channel);
+    handleChange = (event) => {
+        this.setState({
+            name: event.target.value
         })
     }
 
+    handleClick = async () => {
+        var channelURL;
+        if (!this.state.name) {
+            return alert('Enter a channel name');
+        }
+        this.props.sb.OpenChannel.createChannel(this.state.name, null, null, null, (channel, error) => {
+            if (error) { return console.log(error); }
+            channelURL = channel.url;
+            console.log('channelURL: ' + channelURL);
+            this.props.dispatch(clearMessages());
+            this.props.dispatch(setChannelURL(channelURL));
+            this.setState({
+                channelCreated: true
+            })
+        });
+    }
+
     render() {
+        if (this.state.channelCreated) {
+            console.log('expected redirect');
+            console.log(this.props.channelURL);
+            return (
+                <Redirect to={{
+                    pathname: `/chat/${this.props.channelURL}`
+                }} />
+            )
+        }
         return (
             <div>
-                <button onClick={this.handleClick}>Create a Channel</button>
+                <h4>Add a Channel</h4>
+                <Input
+                    size="sm"
+                    placeholder="Enter a channel name"
+                    value={this.state.name}
+                    onChange={this.handleChange}>
+                </Input>
+                <Button size="sm" onClick={this.handleClick}>Create a Channel</ Button>
             </div>
         )
     }
@@ -28,7 +63,8 @@ class CreateChannel extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        sb: state.sbsession.sbsession
+        sb: state.sbsession.sbsession,
+        channelURL: state.channel.channelURL
     }
 }
 
