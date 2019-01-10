f1ba067cf60cba38a525a72892f2b971f8697d18
bloo0178 <bloo0178@gmail.com>
2019-01-10 17:12:59 -0500

    Updates

---
+++ precache-manifest.d189d761b15a6a96f066093074bb5dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41a63393f48a70652e9d",
+    "url": "/chat-client/static/js/runtime~main.41a63393.js"
+  },
+  {
+    "revision": "29da8d30bf11419adc7f",
+    "url": "/chat-client/static/js/main.29da8d30.chunk.js"
+  },
+  {
+    "revision": "1424a48cca83d9f70737",
+    "url": "/chat-client/static/js/1.1424a48c.chunk.js"
+  },
+  {
+    "revision": "29da8d30bf11419adc7f",
+    "url": "/chat-client/static/css/main.6621e181.chunk.css"
+  },
+  {
+    "revision": "fee638da5371401b976a6b93c84d3967",
+    "url": "/chat-client/index.html"
+  }
+];
